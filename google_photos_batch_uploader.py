3078d82fdc62f1b34b081b2be6108a3ddd4d346f
KJ Garcia <kj@psistorm.com>
2023-09-18 10:08:49 -0400

    IMPROVEMENTS FOR RERUNS AFTER ERROR: -Logging skipped files properly. -Identifying and resuming uploads from where it left off during the last run. -Incorporating a global set to track uploaded file hashes to avoid re-uploading in the current session. -Integrating a temporary log file to maintain a log during the current session and then merging it with the main log at the end of the session. -try/except blocks are added to handle exceptions during file uploading and logging. -If the script encounters an error during the upload process, it will log the error along with the hash of the file, and the script will continue to the next file. -In the scan_and_upload_folder function, it checks the last log entry to identify if it ended in an error during the last run and resumes the upload from the last failed file.

--- google_photos_batch_uploader.py
+++ google_photos_batch_uploader.py
@@ -1,8 +1,7 @@
-from google_auth_oauthlib.flow import Flow
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 import requests
 import os
 import time
@@ -11,7 +10,8 @@ from datetime import datetime
 
 # Initialize the log file
 log_file_path = 'upload_log.txt'
-log_file = open(log_file_path, 'a')
+temp_log_file_path = 'temp_upload_log.txt'
+log_file = open(temp_log_file_path, 'a')
 
 
 def log_and_print(message):
@@ -24,7 +24,7 @@ uploaded_file_hashes = set()
 with open(log_file_path, 'r') as f:
     for line in f:
         try:
-            uploaded_file_hashes.add(line.split(', ')[1].rsplit(' ', 1)[1])
+            uploaded_file_hashes.add(line.split('Hash: ')[1].split(',')[0])
         except IndexError:
             continue
 
@@ -52,43 +52,48 @@ service = build('photoslibrary', 'v1', credentials=creds)
 
 def upload_photo(file_path, file_number, total_files):
     start_time = datetime.now()
+    try:
+        with open(file_path, 'rb') as f:
+            file_data = f.read()
+            file_hash = hashlib.md5(file_data).hexdigest()
 
-    with open(file_path, 'rb') as f:
-        file_data = f.read()
-        file_hash = hashlib.md5(file_data).hexdigest()
+        # Skip file if it has already been uploaded
+        if file_hash in uploaded_file_hashes:
+            log_and_print(f"Skipping already uploaded file: {os.path.basename(file_path)}")
+            return
 
-    # Skip file if it has already been uploaded
-    if file_hash in uploaded_file_hashes:
-        log_and_print(f"Skipping already uploaded file: {os.path.basename(file_path)}")
-        return
+        file_name_without_extension = os.path.splitext(os.path.basename(file_path))[0]
+        description = f"Description for {file_name_without_extension}, Hash: {file_hash}"
 
-    file_name_without_extension = os.path.splitext(os.path.basename(file_path))[0]
-    description = f"Description for {file_name_without_extension}, Hash: {file_hash}"
+        upload_url = "https://photoslibrary.googleapis.com/v1/uploads"
+        headers = {
+            "Authorization": f"Bearer {creds.token}",
+            "Content-type": "application/octet-stream",
+            "X-Goog-Upload-File-Name": os.path.basename(file_path),
+            "X-Goog-Upload-Protocol": "raw"
+        }
 
-    upload_url = "https://photoslibrary.googleapis.com/v1/uploads"
-    headers = {
-        "Authorization": f"Bearer {creds.token}",
-        "Content-type": "application/octet-stream",
-        "X-Goog-Upload-File-Name": os.path.basename(file_path),
-        "X-Goog-Upload-Protocol": "raw"
-    }
+        with open(file_path, 'rb') as photo:
+            response = requests.post(upload_url, headers=headers, data=photo)
+            upload_token = response.content.decode('utf-8')
 
-    with open(file_path, 'rb') as photo:
-        response = requests.post(upload_url, headers=headers, data=photo)
-        upload_token = response.content.decode('utf-8')
-
-    create_media_item_request = {
-        "newMediaItems": [
-            {
-                "description": description,
-                "simpleMediaItem": {
-                    "uploadToken": upload_token
+        create_media_item_request = {
+            "newMediaItems": [
+                {
+                    "description": description,
+                    "simpleMediaItem": {
+                        "uploadToken": upload_token
+                    }
                 }
-            }
-        ]
-    }
+            ]
+        }
 
-    response = service.mediaItems().batchCreate(body=create_media_item_request).execute()
+        response = service.mediaItems().batchCreate(body=create_media_item_request).execute()
+    except Exception as e:
+        elapsed_time = datetime.now() - start_time
+        log_and_print(f"Error uploading {os.path.basename(file_path)}: {e}, Hash: {file_hash}, Elapsed time: {elapsed_time}, {file_number}/{total_files}, {total_files}")
+        time.sleep(2)
+        return
 
     elapsed_time = datetime.now() - start_time
     log_message = f"{datetime.now()}, {os.path.basename(file_path)}, {os.path.getsize(file_path)} bytes, Hash: {file_hash}, {time.ctime(os.path.getctime(file_path))}, {time.ctime(os.path.getmtime(file_path))}, {time.ctime(os.path.getatime(file_path))}, {elapsed_time}, {file_number}/{total_files}, {total_files}"
@@ -101,6 +106,20 @@ def scan_and_upload_folder(folder_path):
                   file.lower().endswith(('png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov', 'avi'))]
     total_files = len(file_paths)
 
+    last_line = ""
+    with open(log_file_path, 'r') as f:
+        for last_line in f:
+            pass
+    try:
+        last_file_hash = last_line.split('Hash: ')[1].split(',')[0]
+        if "Error" in last_line and last_file_hash not in uploaded_file_hashes:
+            file_path = [path for path in file_paths if hashlib.md5(open(path, 'rb').read()).hexdigest() == last_file_hash][0]
+            file_paths.remove(file_path)
+            file_paths.insert(0, file_path)
+
+    except IndexError:
+        pass
+
     for file_number, file_path in enumerate(file_paths, start=1):
         upload_photo(file_path, file_number, total_files)
 
@@ -109,3 +128,11 @@ def scan_and_upload_folder(folder_path):
 scan_and_upload_folder(r'D:\RECOVERY\PICS_VIDEOS\FAMILY')
 
 log_file.close()
+
+# Now merge temporary log with the main log
+with open(temp_log_file_path, 'r') as temp_log, open(log_file_path, 'a') as main_log:
+    for line in temp_log:
+        main_log.write(line)
+
+# Remove temporary log file
+os.remove(temp_log_file_path)
